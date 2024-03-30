2b7539a3fb9de83d8263d9d3db7a2dbd15b2a62d
Robert Wang <robwang.us@gmail.com>
2024-03-30 17:30:00 -0500

    react boilerplate

---
+++ safe_route/src/routes/ErrorPage.js
@@ -0,0 +1,5 @@
+function ErrorPage() {
+  return <div>404 not found oops</div>;
+}
+
+export default ErrorPage;
---
+++ safe_route/src/routes/Root.js
@@ -0,0 +1,12 @@
+import classes from "./Root.module.css";
+
+function Root() {
+  return (
+    <div>
+      Safe Route
+      <span className={classes.test}>testing css</span>
+    </div>
+  );
+}
+
+export default Root;
