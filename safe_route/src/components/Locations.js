5b7929af463a07a4d8ee85573689b8a7f38ab6cd
Robert Wang <robwang.us@gmail.com>
2024-04-04 17:36:19 -0500

    edit /locations styling

--- safe_route/src/components/Locations.js
+++ safe_route/src/components/Locations.js
@@ -1,28 +1,32 @@
 import React from "react";
-import "./Locations.css";
 import { FaEdit, FaTrash, FaSearch } from "react-icons/fa";
+import classes from "./Locations.module.css";
 
 function Locations() {
   return (
-    <div className="outer-container">
-        <div className="main-container">
-            <div className="saved-locations">
-                <h2>Saved Locations</h2>
-                <div className="location-item">
-                <div className="location-name">Location 1</div>
-                <div className="icons">
-                    <FaEdit className="edit-icon" />
-                    <FaTrash className="delete-icon" />
-                </div>
-                </div>
+    <div className={classes.outerContainer}>
+      <div className={classes.mainContainer}>
+        <div className={classes.savedLocations}>
+          <h2>Saved Locations</h2>
+          <div className={classes.locationItem}>
+            <div className={classes.locationName}>Location 1</div>
+            <div className={classes.icons}>
+              <FaEdit className={classes.editIcon} />
+              <FaTrash className={classes.deleteIcon} />
             </div>
-            <div className="create-location">
-                <h2>Create New Location</h2>
-                <input type="text" placeholder="Type in address" /><br/><br/>
-                <input type="text" placeholder="Add Custom Name" /><br/><br/>
-                <button>Save Location</button>
-            </div>
+          </div>
         </div>
+        <div className={classes.createLocation}>
+          <h2>Create New Location</h2>
+          <input type="text" placeholder="Type in address" />
+          <br />
+          <br />
+          <input type="text" placeholder="Add Custom Name" />
+          <br />
+          <br />
+          <button>Save Location</button>
+        </div>
+      </div>
     </div>
   );
 }
