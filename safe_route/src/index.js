4a57431a7be67fe82a3febd376fae4d09b82bb73
Benjamin Lindeen <benjaminlindeen@outlook.com>
2024-05-01 13:50:42 -0500

    PWA in progress, Manifest.json working service worker still in progress

--- safe_route/src/index.js
+++ safe_route/src/index.js
@@ -13,4 +13,17 @@ root.render(
   </BrowserRouter>
 );
 
+// Check that service workers are supported
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            })
+            .catch((err) => {
+                console.log('Service worker registration failed: ', err);
+            });
+    });
+}
+
 // reportWebVitals();
