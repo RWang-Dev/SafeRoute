e72fbc54409fd5892efb68cde2f41e7215cd8632
Justin Lee <justinlee01656@gmail.com>
2024-05-05 16:41:32 -0500

    .

--- safe_route/src/routes/Home.js
+++ safe_route/src/routes/Home.js
@@ -28,61 +28,56 @@ function Home() {
         { id: 3, location: "Location 3", description: "Description 3" },
     ];
 
-  console.log(userID, username);
-  return (
-    <div className={classes.homeContainer}>
-      <nav className={classes.navbar}>
-        <ul className={classes.navbarList}>
-          <li>
-            <Link to="/map" className={classes.link}>
-              Map Page
-            </Link>
-          </li>
-          <li>
-            {userID === "Guest" ? null : (
-              <Link to="/locations" className={classes.link}>
-                Saved Locations
-              </Link>
-            )}
-          </li>
-          <li>
-            {userID === "Guest" ? (
-              <a href="/.auth/login/github" className={classes.link}>
-                Login
-              </a>
-            ) : (
-              <a href="/.auth/logout" className={classes.link}>
-                Logout
-              </a>
-            )}
-          </li>
-          {userID !== "Guest" ? (
-            <SubButton className={classes.notificationsButton} />
-          ) : null}
-        </ul>
-      </nav>
-      <div className={classes.contentContainer}>
-        <video
-          autoPlay
-          muted
-          loop
-          id="myVideo"
-          className={classes.backgroundVideo}
-        >
-          <source src="/videos/drone.mp4" type="video/mp4" />
-          console.log("Hello");
-        </video>
-        <div className={classes.safeRouteContainer}>
-          <h1 className={classes.heading}>SafeRoute</h1>
-          <p>
-            Welcome to SafeRoute - your trusted companion for campus safety.
-            With SafeRoute, we've engineered a solution to prioritize student
-            safety by providing real-time tracking and visualizing crime-heavy
-            areas on campus. SafeRoute alerts users when they approach
-            danger-prone zones, enabling proactive route planning for safer
-            journeys.{" "}
-          </p>
-        </div>
+    console.log(userID, username);
+    return (
+        <div className={classes.homeContainer}>
+            <nav className={classes.navbar}>
+                <ul className={classes.navbarList}>
+                    <li>
+                        <Link to="/map" className={classes.link}>
+                            Map Page
+                        </Link>
+                    </li>
+                    <li>
+                        {userID === "Guest" ? null : (
+                            <Link to="/locations" className={classes.link}>
+                                Saved Locations
+                            </Link>
+                        )}
+                    </li>
+                    <li>
+                        {userID === "Guest" ? (
+                            <a href="/.auth/login/github" className={classes.link}>
+                                Login
+                            </a>
+                        ) : (
+                            <a href="/.auth/logout" className={classes.link}>
+                                Logout
+                            </a>
+                        )}
+                    </li>
+                    {userID !== "Guest" ? <SubButton className={classes.notificationsButton} /> : null}
+                </ul>
+            </nav>
+            <div className={classes.contentContainer}>
+                <video
+                    autoPlay
+                    muted
+                    loop
+                    id="myVideo"
+                    className={classes.backgroundVideo}
+                    onContextMenu={(e) => e.preventDefault()}
+                >
+                    <source src="/videos/drone.mp4" type="video/mp4" />
+                </video>
+
+                <div className={classes.safeRouteContainer}>
+                    <h1 className={classes.heading}>SafeRoute</h1>
+                    <p>
+                        Welcome to SafeRoute - your trusted companion for campus safety. With SafeRoute, we've engineered a solution to prioritize student safety by providing real-time tracking and visualizing crime-heavy areas on campus. SafeRoute alerts users when they approach danger-prone zones,
+                        enabling proactive route planning for safer journeys.{" "}
+                    </p>
+                </div>
 
                 {/* Recent Crime Incidents Section
         <div className={classes.crimeContainer}>
